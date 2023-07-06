920fed02758d83f3b175ab275fedd247e0c557b3
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2023-07-06 09:05:52 +0100

    Add project files.

---
+++ AccessibleSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AccessibleSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AccessibleSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AccessibleSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
