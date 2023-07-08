357928c3232ebf88ec6514807b64d709d423a00a
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2023-07-08 08:56:40 +0100

    Add sample controls to the main MAUI page.

--- AccessibleSample/MainPage.xaml.cs
+++ AccessibleSample/MainPage.xaml.cs
@@ -1,10 +1,41 @@
-﻿namespace AccessibleSample;
+﻿using System.Collections.ObjectModel;
+using Windows.ApplicationModel.Resources;
+
+namespace AccessibleSample;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    public SampleItemViewModel ViewModel { get; set; }
+}
+
+public class SampleItemViewModel
+{
+    private ObservableCollection<SampleItem> sampleItems = new ObservableCollection<SampleItem>();
+    public ObservableCollection<SampleItem> SampleItems { get { return this.sampleItems; } }
+
+    public SampleItemViewModel()
+    {
+        //        var loader = new ResourceLoader();
+
+        this.sampleItems.Add(new SampleItem()
+        {
+            Name = "First"
+        }); ;
+
+        this.sampleItems.Add(new SampleItem()
+        {
+            Name = "Second"
+        });
+    }
+}
+
+public class SampleItem
+{
+    public string Name { get; set; }
 }
 
